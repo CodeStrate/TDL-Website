2b8bd78398afa465fc555befc8c147c47fa4ece2
CodeStrate <56911705+CodeStrate@users.noreply.github.com>
2022-11-09 00:04:32 +0530

    Working on Home Section
    - Responsive Login-Form done
    - Worked out minor kinks with sizing.

--- main.js
+++ main.js
@@ -5,10 +5,21 @@ $(document).ready(function(){
         $('.navbar').toggleClass('nav-toggle');
     });
 
+    $('#login').click(function(){
+        $('.login-form').addClass('popup');
+    });
+
+    $('.login-form form .fa-times').click(function(){
+        $('.login-form').removeClass('popup');
+    });
+
+
     $(window).on('load scroll', function(){
 
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        $('.login-form').removeClass('popup');
 
     });
 
